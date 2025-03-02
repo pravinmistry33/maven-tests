481e5e4c2f3f1a7b6c0fa3a1c9d2b83a142983e0
Pravin Mistry <pravinmistry@Pravins-MacBook-Air.local>
2025-03-02 16:47:22 +0530

    Updated compose file to run selenium in grid

--- src/test/java/com/automation/tests/GoogleSearchTest.java
+++ src/test/java/com/automation/tests/GoogleSearchTest.java
@@ -7,18 +7,22 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Arrays;
 
 public class GoogleSearchTest {
-    WebDriver driver;
+    //WebDriver driver;
+    RemoteWebDriver driver;
     GoogleSearchPage searchPage;
 
     @BeforeClass
@@ -26,14 +30,17 @@ public class GoogleSearchTest {
         WebDriverManager.chromedriver().clearDriverCache().setup();
         ChromeOptions chromeOptions = new ChromeOptions();
         chromeOptions.addArguments("--disable-blink-features=AutomationControlled");
-        Path tempDir = Files.createTempDirectory("chrome-profile");
-        chromeOptions.addArguments("--user-data-dir=" + tempDir.toAbsolutePath().toString());
-        chromeOptions.addArguments("--headless=new","--disable-gpu", "--no-sandbox", "--disable-dev-shm-usage", "--start-maximized");
+        //Path tempDir = Files.createTempDirectory("chrome-profile");
+        //chromeOptions.addArguments("--user-data-dir=" + tempDir.toAbsolutePath().toString());
+        chromeOptions.addArguments("--remote-allow-origins=*,--headless=new","--disable-gpu", "--no-sandbox", "--disable-dev-shm-usage", "--start-maximized");
         chromeOptions.addArguments("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36");
         chromeOptions.setExperimentalOption("excludeSwitches", Arrays.asList("enable-automation"));
         chromeOptions.setExperimentalOption("useAutomationExtension", false);
-        driver = new ChromeDriver(chromeOptions);
-        driver.manage().window().maximize();
+//        driver = new ChromeDriver(chromeOptions);
+//        driver.manage().window().maximize();
+//        searchPage = new GoogleSearchPage(driver);
+
+        driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), chromeOptions);
         searchPage = new GoogleSearchPage(driver);
     }
 
