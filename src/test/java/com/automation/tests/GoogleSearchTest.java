8d863c51cbcaea44f9728863e0b9bee7d15486c4
Pravin Mistry <pravinmistry@Pravins-MacBook-Air.local>
2025-03-03 21:32:55 +0530

    Added firefox browser test

--- src/test/java/com/automation/tests/GoogleSearchTest.java
+++ src/test/java/com/automation/tests/GoogleSearchTest.java
@@ -3,44 +3,63 @@ package com.automation.tests;
 import com.automation.pages.GoogleSearchPage;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.time.Duration;
 import java.util.Arrays;
+import java.util.Random;
 
 public class GoogleSearchTest {
     //WebDriver driver;
     RemoteWebDriver driver;
     GoogleSearchPage searchPage;
 
+    @Parameters("browser")
     @BeforeClass
-    public void setup() throws IOException {
-        WebDriverManager.chromedriver().clearDriverCache().setup();
-        ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.addArguments("--disable-blink-features=AutomationControlled");
-        //Path tempDir = Files.createTempDirectory("chrome-profile");
-        //chromeOptions.addArguments("--user-data-dir=" + tempDir.toAbsolutePath().toString());
-        chromeOptions.addArguments("--remote-allow-origins=*,--headless=new","--disable-gpu", "--no-sandbox", "--disable-dev-shm-usage", "--start-maximized");
-        chromeOptions.addArguments("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36");
-        //chromeOptions.setExperimentalOption("excludeSwitches", Arrays.asList("enable-automation"));
-        //chromeOptions.setExperimentalOption("useAutomationExtension", false);
-        //driver = new ChromeDriver(chromeOptions);
-        //driver.manage().window().maximize();
-        //searchPage = new GoogleSearchPage(driver);
-
-        driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), chromeOptions);
+    public void setup(String browser) throws IOException, InterruptedException {
+        String seleniumGridURL = "http://localhost:4444/wd/hub";  // Change if using a different grid URL
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        if (browser.equalsIgnoreCase("chrome")) {
+            capabilities.setBrowserName("chrome");
+            WebDriverManager.chromedriver().clearDriverCache().setup();
+            ChromeOptions chromeOptions = new ChromeOptions();
+            chromeOptions.addArguments("--disable-blink-features=AutomationControlled");
+            chromeOptions.addArguments("--remote-allow-origins=*","--headless=new","--disable-gpu", "--no-sandbox", "--disable-dev-shm-usage", "--start-maximized");
+            chromeOptions.addArguments("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36");
+            //driver = new RemoteWebDriver(new URL(seleniumGridURL), chromeOptions);
+            driver = new ChromeDriver(chromeOptions);
+        }else {
+            capabilities.setBrowserName("firefox");
+            WebDriverManager.firefoxdriver().clearDriverCache().setup();
+            FirefoxOptions firefoxOptions = new FirefoxOptions();
+            firefoxOptions.addArguments("--disable-blink-features=AutomationControlled");
+            firefoxOptions.addArguments("--headless","--disable-gpu", "--no-sandbox", "--disable-dev-shm-usage", "--start-maximized","--disable-extensions");
+            firefoxOptions.addArguments("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36");
+            //firefoxOptions.addPreference("general.useragent.override","Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36");
+            firefoxOptions.setProfile(new FirefoxProfile(new File("/Users/pravinmistry/Library/Application Support/Firefox/Profiles/7s2j4yfa.default-release-1737955747675")));
+            //driver = new RemoteWebDriver(new URL(seleniumGridURL), firefoxOptions);
+            driver = new FirefoxDriver(firefoxOptions);
+        }
         searchPage = new GoogleSearchPage(driver);
     }
 
@@ -51,7 +70,7 @@ public class GoogleSearchTest {
             searchPage.enterSearchQuery("Maven Selenium Automation");
             searchPage.randomDelay();
             searchPage.clickSearchButton();
-            Thread.sleep(3000);
+            searchPage.randomDelay();
             System.out.println(driver.getTitle());
             Assert.assertTrue(driver.getTitle().contains("Maven Selenium Automation"));
         } catch (AssertionError e){
